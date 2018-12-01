379c154e5ad54a4363c8ced009aa1d630035eca5
Brad Davies <varbrad@users.noreply.github.com>
2018-12-01 10:27:23 +0000

    Move inputs out of js file

--- js/utils/readFile.js
+++ js/utils/readFile.js
@@ -2,6 +2,6 @@ import fs from 'fs';
 import path from 'path';
 
 export default (filename, transform) =>
-  fs.readFileSync(path.join(__dirname, '../input/', filename), {
+  fs.readFileSync(path.join(__dirname, '../../inputs/', filename), {
     encoding: 'utf-8',
   });
