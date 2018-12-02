c2925041e032d937538fa6819221a590ce820239
Brad Davies <varbrad@users.noreply.github.com>
2018-12-02 09:46:35 +0000

    More reduction of code

--- js/days/day2.js
+++ js/days/day2.js
@@ -1,5 +1,4 @@
 import countBy from 'lodash/countBy';
-import difference from 'lodash/difference';
 import readFile from '../utils/readFile';
 
 const input = readFile('input-2.txt')
@@ -16,16 +15,8 @@ export const part1 = () =>
     )
     .reduce((p, c) => c * p, 1);
 
-const diff = (a, b) => {
-  const common = a.filter((char, i) => char === b[i]);
-  return common.length + 1 === a.length ? common.join('') : false;
-};
-
-export const part2 = () => {
-  for (let i = 0; i < input.length - 1; ++i)
-    for (let j = i + 1; j < input.length; ++j) {
-      const [a, b] = [input[i], input[j]];
-      const common = diff(a, b);
-      if (common) return common;
-    }
-};
+export const part2 = () =>
+  input.findCompare((a, b) => {
+    const common = a.filter((char, i) => char === b[i]);
+    return common.length + 1 === a.length && common.join('');
+  });
