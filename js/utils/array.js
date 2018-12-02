2b1c5d2ffe930da9ac7934d7d25fdd3cd3cc13fb
Brad Davies <varbrad@users.noreply.github.com>
2018-12-02 09:46:19 +0000

    Add some util methods to array prototpe

---
+++ js/utils/array.js
@@ -0,0 +1,28 @@
+Object.defineProperty(Array.prototype, 'compare', {
+  value: function(fn) {
+    const r = [];
+    const len = this.length;
+    for (let i = 0; i < len - 1; ++i) {
+      for (let j = i + 1; j < len; ++j) {
+        const [a, b] = [this[i], this[j]];
+        const result = fn(a, b, i, j);
+        result && r.push(result);
+      }
+    }
+    return r;
+  },
+});
+
+Object.defineProperty(Array.prototype, 'findCompare', {
+  value: function(fn) {
+    const len = this.length;
+    for (let i = 0; i < len - 1; ++i) {
+      for (let j = i + 1; j < len; ++j) {
+        const [a, b] = [this[i], this[j]];
+        const result = fn(a, b, i, j);
+        if (result) return result;
+      }
+    }
+    return false;
+  },
+});
