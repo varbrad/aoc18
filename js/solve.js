5e3b9ea565f0ea43f0801c2070c05dde49f62629
Brad Davies <varbrad@users.noreply.github.com>
2018-12-03 21:03:05 +0000

    Make logging great again

--- js/solve.js
+++ js/solve.js
@@ -16,8 +16,15 @@ const logTime = (day, part, fn) => {
   const time = process.hrtime();
   const result = fn();
   const elapsed = process.hrtime(time);
-  log(day, part, result, (elapsed[1] / 1000000).toFixed(1));
+  return [day, part, result, (elapsed[1] / 1000000).toFixed(1)];
 };
 
-if (part === 0 || part === 1) logTime(day, 1, part1);
-if (part === 0 || part === 2) logTime(day, 2, part2);
+const logs = [];
+if (part === 0 || part === 1) logs.push(logTime(day, 1, part1));
+if (part === 0 || part === 2) logs.push(logTime(day, 2, part2));
+
+const maxAnswer = logs
+  .map(data => String(data[2]).length)
+  .reduce((acc, cur) => (cur > acc ? cur : acc), -1);
+
+logs.forEach(result => log(...result, maxAnswer));
--- js/utils/log.js
+++ js/utils/log.js
@@ -1,10 +1,10 @@
 import chalk from 'chalk';
 
-export default (day, part, result, elapsed = -1) => {
+export default (day, part, result, elapsed, maxLen) => {
   console.log(
     chalk.blue(`Day ${day},`),
     chalk.yellow(`Part ${part}:`),
-    chalk.green(result),
+    chalk.green(String(result).padEnd(maxLen, ' ')),
     elapsed > -1 && chalk.red(`(${elapsed.padStart(5, ' ')} ms)`),
   );
 };
